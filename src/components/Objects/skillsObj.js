b75910b7a80b2ddda95137c086628b93ba549994
Abdifatah Osman <137517451+AbdifatahCodes@users.noreply.github.com>
2023-07-06 16:20:26 +0300

    Updated the skillsObj list to include keys for react's rendering of the list.

--- src/components/Objects/skillsObj.js
+++ src/components/Objects/skillsObj.js
@@ -1,11 +1,38 @@
 export const skills = [
-  'HTML5',
-  'TailwindCSS',
-  'Javascript ES6',
-  'ReactJS',
-  'Typescript',
-  'NextJS',
-  'NodeJS',
-  'Golang',
-  'NoSql & Git',
-];
+  {
+    id: 0,
+    name: 'HTML5',
+  },
+  {
+    id: 1,
+    name: 'TailwindCSS',
+  },
+  {
+    id: 2,
+    name: 'Javascript ES6',
+  },
+  {
+    id: 3,
+    name: 'ReactJS',
+  },
+  {
+    id: 4,
+    name: 'Typescript',
+  },
+  {
+    id: 5,
+    name: 'NextJS',
+  },
+  {
+    id: 6,
+    name: 'NodeJS',
+  },
+  {
+    id: 7,
+    name: 'Golang',
+  },
+  {
+    id: 8,
+    name: 'NoSql & Git',
+  },
+]
