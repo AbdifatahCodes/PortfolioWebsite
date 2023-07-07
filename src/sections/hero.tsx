3b6346e1d719e72e8694a9fe3c76789af62cf10d
Abdifatah Osman <137517451+AbdifatahCodes@users.noreply.github.com>
2023-07-07 10:50:32 +0300

    Polishing up hero section.

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
             <The3Balls/>
           </div>
           <div className='px-8 space-y-1 mb-8'>
-            <p className='text-gray-dark dark:text-gray-main font-roboto text-2xl'>Greetings! I'm a passionate Full-Stack Web Developer skilled in crafting elegant, powerful applications. With expertise in both Front-End and Back-End, I find joy in the magic of Back-End development. As an architect, I explore the endless possibilities of the Cloud. Linux enthusiast, privacy advocate, and desktop minimalist.</p>
+            <p className='text-gray-dark dark:text-gray-main font-roboto text-2xl'>Greetings! I'm a passionate Full-Stack Web Developer skilled in crafting elegant, powerful applications. With expertise in both Front-End and Back-End, I find joy in the magic of Back-End development. As an architect, I explore the endless possibilities of the Cloud with code. Linux enthusiast, Open-source contributor, Privacy advocate, and Desktop minimalist.</p>
             <p className='text-gray-dark dark:text-gray-main font-roboto text-2xl'>But it's not all work and no play for me! When I'm not busy building awesome applications, I find solace in immersing myself in philosophical masterpieces like Plato's Republic. Additionally, I stay engaged with the latest social dramas that shape our zeitgeist. And of course, no day is complete without a healthy dose of hip hop music. On my blog, I delve into these passions and more, sharing my insights and experiences.</p>
           </div>
         </div>
