983dbb7d57dee789821ce777cbe5edf9aa7a3e87
Abdifatah Osman <137517451+AbdifatahCodes@users.noreply.github.com>
2023-07-16 17:40:02 +0300

    Changed <a> to <Link> tags, changed ESLint rules and added firebase project and set-up firebase github auto-deploy.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { FiTerminal } from 'react-icons/fi'
 import { MdMenu, MdCancelPresentation, } from 'react-icons/md'
 import ThemeToggle from '@/components/themeToggle'
+import Link from 'next/link'
 
 export default function Navbar(props: any) {
 	const [navUlIsOpen, setNavUlIsOpen] = useState(true);
@@ -17,7 +18,7 @@ export default function Navbar(props: any) {
 			</Head>
 			<main>
 				<nav id='header' className='flex bg-white dark:bg-black-main border p-4 sm:p-8 sm:px-14 xl:px-28 justify-between items-center space-x-4'>
-					<a href="/">
+					<Link href="/">
 						<div className='flex items-center space-x-3'>
 							<div className='shrink-0 text-gray-dark dark:text-gray-main'>
 								<FiTerminal
@@ -29,16 +30,16 @@ export default function Navbar(props: any) {
 								<p className='w-max hidden sm:block'>ABDIFATAH CODES</p>
 							</div>
 						</div>
-					</a>
+					</Link>
 					<div className='flex space-x-6'>
 						<div className='hidden lg:block'>
 							<ul className={`w-full flex-grow lg:flex lg:items-center lg:w-auto`}>
 								<li className={`text-black dark:text-white font-robotocon text-xl mt-1 space-x-6`}>
-									<a href="#about-section">About</a>
-									<a href="#projects-section">Projects</a>
-									<a href="#">Blog</a>
-									<a href="#">Resume</a>
-									<a href="#contact-section">Contact</a>
+									<Link href="#about-section">About</Link>
+									<Link href="#projects-section">Projects</Link>
+									<Link href="#">Blog</Link>
+									<Link href="#">Resume</Link>
+									<Link href="#contact-section">Contact</Link>
 								</li>
 							</ul>
 						</div>
@@ -67,11 +68,11 @@ export default function Navbar(props: any) {
 				<div>
 					<ul className={`flex-grow lg:items-center lg:w-auto lg:hidden border rounded-lg mx-2 my-2 py-2 bg-white dark:bg-black-main ${navUlIsOpen ? "hidden" : "block"}`}>
 						<li className={`text-black dark:text-white font-robotocon px-1 sm:px-2 text-xl space-y-1 flex flex-col`}>
-							<a href="#about-section" className='bg-gray dark:bg-black-bg px-2 sm:px-4'>About</a>
-							<a href="#projects-section" className='px-2 sm:px-4'>Projects</a>
-							<a href="#" className='bg-gray dark:bg-black-bg px-2 sm:px-4'>Blog</a>
-							<a href="#" className='px-2 sm:px-4'>Resume</a>
-							<a href="#contact-section" className='bg-gray dark:bg-black-bg px-2 sm:px-4'>Contact</a>
+							<Link href="#about-section" className='bg-gray dark:bg-black-bg px-2 sm:px-4'>About</Link>
+							<Link href="#projects-section" className='px-2 sm:px-4'>Projects</Link>
+							<Link href="#" className='bg-gray dark:bg-black-bg px-2 sm:px-4'>Blog</Link>
+							<Link href="#" className='px-2 sm:px-4'>Resume</Link>
+							<Link href="#contact-section" className='bg-gray dark:bg-black-bg px-2 sm:px-4'>Contact</Link>
 						</li>
 					</ul>
 				</div>
