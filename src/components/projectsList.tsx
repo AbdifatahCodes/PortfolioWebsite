f9c9e7be0372057c2c150372803ed12e601c25a7
Abdifatah Osman <137517451+AbdifatahCodes@users.noreply.github.com>
2023-07-20 10:47:02 +0300

    Made myProjects section fully responsive.

--- src/components/projectsList.tsx
+++ src/components/projectsList.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { projects } from './Objects/projectsObj.js'
 import { BsFillCircleFill, BsFillClockFill, } from 'react-icons/bs'
+import { FiExternalLink } from 'react-icons/fi'
 
 export default function ProjectsList() {
   const projectsItems = projects.map(project => (
-    <div key={project.id} className='flex flex-col space-y-2 mr-20'>
+    <div key={project.id} className='flex flex-col space-y-2 mr-0'>
       <div className='bg-black dark:bg-white rounded-md'>
         <Image
           src={project.imgUrl}
@@ -15,7 +16,7 @@ export default function ProjectsList() {
           blurDataURL={project.imgBlurDataUrl}
         />
       </div>
-      <p className='text-gray-dark dark:text-gray-main font-oswald font-medium text-5xl'>{ project.name }</p>
+      <p className='text-gray-dark dark:text-gray-main font-oswald font-medium text-2xl sm:text-3xl md:text-5xl'>{ project.name }</p>
       <div className='flex flex-row space-x-2'>
         {
           project.statusCode === "live" ?
@@ -31,16 +32,22 @@ export default function ProjectsList() {
         }
         <p className='italic text-gray-dark dark:text-gray-main font-roboto font-medium'>{ project.status }</p>
       </div>
-      <p className='text-gray-dark dark:text-gray-main font-roboto'>{ project.description }</p>
-      <div className='flex flex-row justify-between text-gray-dark dark:text-gray-main font-roboto font-bold italic uppercase text-1xl'>
-        <a href={project.blogUrl}>Read More</a>
-        <a href={project.liveUrl}>View Project</a>
+      <p className='text-gray-dark dark:text-gray-main font-roboto text-sm md:text-base'>{ project.description }</p>
+      <div className='flex flex-row justify-between text-gray-dark dark:text-gray-main font-roboto font-bold italic text-xs md:text-base'>
+        <a href={project.blogUrl} className='flex flex-row space-x-1'>
+          <span>Read More</span>
+          <FiExternalLink />
+        </a>
+        <a href={project.liveUrl} className='flex flex-row space-x-1'>
+          <span>View Project</span>
+          <FiExternalLink />
+        </a>
       </div>
     </div>
   ));
   
   return (
-    <div className='p-4 grid grid-cols-2 gap-4'>
+    <div className='px-5 md:px-10 lg:mr-5 grid grid-cols-2 gap-3 md:gap-5'>
       {projectsItems}
     </div>
   )
--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -2,13 +2,13 @@ import ProjectsList from '@/components/projectsList'
 
 export default function ProjectsSection() {
   return (
-    <div id='projects-section' className='max-w-5xl mx-auto bg-white dark:bg-black rounded-3xl shadow-lg  items-center border'>
-      <div className='flex flex-col px-8 mb-8'>
+    <div id='projects-section' className='mx-2 md:max-w-3xl lg:max-w-4xl xl:max-w-5xl md:mx-auto bg-white dark:bg-black rounded-xl sm:rounded-3xl shadow-lg  items-center border'>
+      <div className='flex flex-col mb-6 md:mb-12'>
         <div className='flex flex-row justify-between'>
-          <p className='mt-6 mb-4 text-6xl font-oswald font-medium text-black dark:text-white'>My Endeavors</p>
+          <p className='px-4 md:px-8 mt-4 mb-2 sm:mb-5 text-3xl sm:text-4xl md:text-6xl font-oswald font-medium text-black dark:text-white'>My Endeavors</p>
         </div>
-        <div className='space-y-1 mb-4'>
-          <p className='text-gray-dark dark:text-gray-main font-roboto text-2xl'>In my work, I bring ideas to life through creativity, precision, and attention to detail. Each endeavor is a testament to my passion for problem-solving and my commitment to delivering exceptional results. From concept to execution, I approach projects with a meticulous mindset, crafting solutions that inspire and leave a lasting impact.</p>
+        <div className='px-4 md:px-8 mb-2 md:mb-4'>
+          <p className='text-gray-dark dark:text-gray-main font-roboto text-base sm:text-xl md:text-2xl'>In my work, I bring ideas to life through creativity, precision, and attention to detail. Each endeavor is a testament to my passion for problem-solving and my commitment to delivering exceptional results. From concept to execution, I approach projects with a meticulous mindset, crafting solutions that inspire and leave a lasting impact.</p>
         </div>
         <ProjectsList />
       </div>
