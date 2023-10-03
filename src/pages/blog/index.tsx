ccd9f8c76ea66baae010c1039a9ea19e8bbeb23e
Abdifatah Osman <137517451+AbdifatahCodes@users.noreply.github.com>
2023-10-03 14:42:42 +0300

    Fully refactored and added unit tests on the whole codebase. Now, the only thing that remains is integrating MDX as my blogging engine and bringing to life my contact section.

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,23 +1,37 @@
-import Layout from '@/layouts/layout'
-import BlogExplorer from '@/sections/BlogExplorer/blogExplorer'
-import BlogHeroSection from '@/sections/BlogHero/blogHero'
-import BlogSummary from '@/sections/BlogSummary/blogSummary'
+import Layout from '@/layouts/layout';
+import BlogExplorer from '@/sections/BlogExplorer/blogExplorer';
+import BlogHeroSection from '@/sections/BlogHero/blogHero';
+import BlogSummary from '@/sections/BlogSummary/blogSummary';
 
 export default function Blog() {
   return (
     <Layout title='Blog'>
+      {/* Main Blog Container */}
       <div id='blog' className='space-y-6 lg:space-y-12'>
+        
+        {/* Blog Hero Section */}
         <BlogHeroSection />
+        
+        {/* Watermark */}
         <div id='Watermark'>
-          <p className='fixed bottom-10 left-0 -z-10 font-quicksand font-extrabold text-9xl dark:text-watermark-dark text-watermark-light'
-          >Abdifatah's Blog.</p>
+          <p className='fixed bottom-10 left-0 -z-10 font-quicksand font-extrabold text-9xl dark:text-watermark-dark text-watermark-light'>
+            Abdifatah's Blog.
+          </p>
         </div>
-        <div id='divider' className='p-1 lg:p-4'></div>
+        
+        {/* Divider */}
+        <div className='p-1 lg:p-4'></div>
+        
+        {/* Blog Explorer Section */}
         <BlogExplorer />
-        <div id='divider' className='p-1'></div>
+        
+        {/* Divider */}
+        <div className='p-1'></div>
+        
+        {/* Blog Summary Section */}
         <BlogSummary />
+        
       </div>
     </Layout>
-
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,58 @@
-import Layout from '@/layouts/layout'
-import AboutSection from '@/sections/About/about'
-import HeroSection from '@/sections/Hero/hero'
-import MyBackgroundSection from '@/sections/MyBackground/myBackground'
-import ProjectsSection from '@/sections/Projects/projects'
-import SkillsSection from '@/sections/Skills/skills'
-import SummarySection from '@/sections/Summary/summary'
+import Layout from '@/layouts/layout';
+import AboutSection from '@/sections/About/about';
+import HeroSection from '@/sections/Hero/hero';
+import MyBackgroundSection from '@/sections/MyBackground/myBackground';
+import ProjectsSection from '@/sections/Projects/projects';
+import SkillsSection from '@/sections/Skills/skills';
+import SummarySection from '@/sections/Summary/summary';
 
 export default function Home() {
   return (
     <Layout title='Abdifatah Codes'>
+      {/* Main Home Container */}
       <div id='home' className='space-y-6 lg:space-y-12'>
+        
+        {/* Hero Section */}
         <HeroSection />
+        
+        {/* Watermark */}
         <div id='Watermark'>
-          <p className='fixed bottom-10 left-0 -z-10 font-quicksand font-extrabold text-9xl dark:text-watermark-dark text-watermark-light'
-          >Abdifatah Codes.</p>
+          <p className='fixed bottom-10 left-0 -z-10 font-quicksand font-extrabold text-9xl dark:text-watermark-dark text-watermark-light'>
+            Abdifatah Codes.
+          </p>
         </div>
-        <div id='divider' className='p-1 lg:p-4'></div>
+        
+        {/* Divider */}
+        <div className='p-1 lg:p-4'></div>
+        
+        {/* Skills Section */}
         <SkillsSection />
-        <div id='divider' className='p-1'></div>
+        
+        {/* Divider */}
+        <div className='p-1'></div>
+        
+        {/* Projects Section */}
         <ProjectsSection />
-        <div id='divider' className='p-1'></div>
+        
+        {/* Divider */}
+        <div className='p-1'></div>
+        
+        {/* About Section */}
         <AboutSection />
-        <div id='divider' className='p-1'></div>
+        
+        {/* Divider */}
+        <div className='p-1'></div>
+        
+        {/* My Background Section */}
         <MyBackgroundSection />
-        <div id='divider' className='p-1'></div>
+        
+        {/* Divider */}
+        <div className='p-1'></div>
+        
+        {/* Summary Section */}
         <SummarySection />
+        
       </div>
     </Layout>
-
-  )
+  );
 }
