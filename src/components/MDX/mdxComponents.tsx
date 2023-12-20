645ef40b4ea7520414aa8890eb984b6b643d450b
Abdifatah Osman <137517451+AbdifatahCodes@users.noreply.github.com>
2023-12-20 21:55:57 +0300

    Fixed the Error: React Hook useState is called in function code that is neither a React function component nor a custom React Hook function. React component names must start with an uppercase letter. React Hook names must start with the word use.  react-hooks/rules-of-hooks.

--- src/components/MDX/mdxComponents.tsx
+++ src/components/MDX/mdxComponents.tsx
@@ -3,6 +3,53 @@ const { Prism: SyntaxHighlighter } = require('react-syntax-highlighter');
 const { dracula } = require('react-syntax-highlighter/dist/cjs/styles/prism');
 import { LuClipboard, LuClipboardCheck } from "react-icons/lu";
 
+const CodeBlock = ({ children, ...props }: HTMLAttributes<HTMLParagraphElement>) => {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = async (children: any) => {
+    if ('clipboard' in navigator) {
+      await navigator.clipboard.writeText(children);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+    }
+  };
+
+  // Check if it is a block code (has a className)
+  if (props.className && props.className.startsWith('language-')) {
+    const language = props.className.replace(/language-/, '');
+    return (
+      <div className='flex flex-col w-auto relative space-y-5'>
+        <div className='copy-code bg-black-bg flex flex-row justify-between absolute w-full py-2 rounded-t text-xs px-4'>
+            <p>{language}</p>
+            <button
+              className='flex flex-row gap-1'
+              onClick={() => copyToClipboard(children)}
+            >
+              {!copied ? (
+                <>
+                  <LuClipboard size={14} />
+                  <p>Copy</p>
+                </>
+              ) : (
+                <>
+                  <LuClipboardCheck size={14} />
+                  <p>Copied!</p>
+                </>
+              )}
+            </button>
+        </div>
+        <div></div>
+        <SyntaxHighlighter className='code flex-grow' style={dracula} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      </div>
+    );
+  }
+
+  // Fallback for inline code (no className)
+  return <code className='bg-code-dark-bg p-1 rounded'>{children}</code>;
+};
+
 const MDXComponents = {
   h1: ({ children, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
     <h1 className='text-5xl mb-10 font-medium font-oswald' {...props}>{children}</h1>
@@ -19,52 +66,7 @@ const MDXComponents = {
   ul: ({ children, ...props }: HTMLAttributes<HTMLUListElement>) => (
     <ul className='space-y-1' {...props}>{children}</ul>
   ),
-  code: ({ children, ...props }: HTMLAttributes<HTMLParagraphElement>) => {
-    const [copied, setCopied] = useState(false);
-
-    const copyToClipboard = async (children: any) => {
-      if ('clipboard' in navigator) {
-        await navigator.clipboard.writeText(children);
-        setCopied(true);
-        setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
-      }
-    };
-
-    // Check if it is a block code (has a className)
-    if (props.className && props.className.startsWith('language-')) {
-      const language = props.className.replace(/language-/, '');
-      return (
-        <div className='flex flex-col w-auto relative space-y-5'>
-          <div className='copy-code bg-black-bg flex flex-row justify-between absolute w-full py-2 rounded-t text-xs px-4'>
-              <p>{language}</p>
-              <button
-                className='flex flex-row gap-1'
-                onClick={() => copyToClipboard(children)}
-              >
-                {!copied ? (
-                  <>
-                    <LuClipboard size={14} />
-                    <p>Copy</p>
-                  </>
-                ) : (
-                  <>
-                    <LuClipboardCheck size={14} />
-                    <p>Copied!</p>
-                  </>
-                )}
-              </button>
-          </div>
-          <div></div>
-          <SyntaxHighlighter className='code flex-grow' style={dracula} language={language}>
-            {children}
-          </SyntaxHighlighter>
-        </div>
-      );
-    }
-
-    // Fallback for inline code (no className)
-    return <code className='bg-code-dark-bg p-1 rounded'>{children}</code>;
-  },
+  code: CodeBlock,
   // ... other custom components
 };
 
